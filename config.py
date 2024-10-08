c3df0fa2600c89c6232d803c2711bd00b66a80cd
Ryan Oelkers <ryanjamesoelkers@gmail.com>
2024-10-08 15:49:46 -0500

    version 0 of the GUI completed

--- config.py
+++ config.py
@@ -3,55 +3,62 @@
 
 class Configuration:
 
-    # these are star field specific informations
-    FIELD_NAME = "Albireo"
-    RA_DEG = 292.6803333
-    DEC_DEG = 27.9596750
+    # these are star field specific information
+    FIELD_NAME = "WASP-52"
+    RA_DEG = 348.4948229
+    DEC_DEG = 8.7612697
 
     # update the offsets between the center of each guide camera and the fiber in mm
     # positive is right or up, negative is left or down
-    OFFSET_X_1 = 20
-    OFFSET_Y_1 = -20
+    OFFSET_X_1 = 5
+    OFFSET_Y_1 = -5
 
-    OFFSET_X_2 = -10
-    OFFSET_Y_2 = -20
+    OFFSET_X_2 = -2
+    OFFSET_Y_2 = -2
 
-    OFFSET_X_3 = -15
-    OFFSET_Y_3 = 20
+    OFFSET_X_3 = -10
+    OFFSET_Y_3 = 3
 
-    OFFSET_X_4 = 15
-    OFFSET_Y_4 = 20
+    OFFSET_X_4 = 5
+    OFFSET_Y_4 = 10
 
     OFFSETS = [[OFFSET_X_1, OFFSET_X_2, OFFSET_X_3, OFFSET_X_4], [OFFSET_Y_1, OFFSET_Y_2, OFFSET_Y_3, OFFSET_Y_4]]
 
-    # these are computer specific informations
+    # these are computer specific information
     MACHINE = "moon"
-    MACHINE_TYPE = "windows"
+    MACHINE_TYPE = "linux"
 
     if MACHINE_TYPE == "windows":
         DIRECTORY_DELIM = "\\"
     else:
         DIRECTORY_DELIM = "/"
 
-    # this is instrument specific information
-    DFPS_PIXEL_SCALE = 7.4  # micron / pixel
-    DFPS_GUIDE_CAMERA_SIZE = 30  # arcsec in diameter
+    # telescope specific information
     OTTO_STRUVE_PLATE_SCALE = 7.23  # arcsec / mm
     OTTO_STRUVE_FIELD_OF_VIEW_ARCMIN = 7  # arcmin
     OTTO_STRUVE_FIELD_OF_VIEW_DEG = OTTO_STRUVE_FIELD_OF_VIEW_ARCMIN / 60.
 
+    # this is instrument specific information
+    UM_TO_MM = 0.001
+    DFPS_PIXEL_SCALE = 7.4  # micron / pixel
+    DFPS_GUIDE_CAMERA_X = 520  # pixels
+    DFPS_GUIDE_CAMERA_Y = 700  # pixels
+    DFPS_GUIDE_CAMERA_FOV_X = DFPS_GUIDE_CAMERA_X * DFPS_PIXEL_SCALE * UM_TO_MM * OTTO_STRUVE_PLATE_SCALE / 3600
+    DFPS_GUIDE_CAMERA_FOV_Y = DFPS_GUIDE_CAMERA_X * DFPS_PIXEL_SCALE * UM_TO_MM * OTTO_STRUVE_PLATE_SCALE / 3600
+    DFPS_GUIDE_CAMERA_DIST = 45  # mm
+
     # these are TIC search specific information
     SEARCH_RADIUS_ARCMIN = OTTO_STRUVE_FIELD_OF_VIEW_ARCMIN + 3  # add an X arcmin buffer
     SEARCH_RADIUS_DEG = SEARCH_RADIUS_ARCMIN / 60. / 2.  # convert to degrees and divide by 2 to get the radius
-    MAGNITUDE_CUTOFF = 14  # this is the lower limit of the magnitudes to keep
+    MAGNITUDE_CUTOFF = 16  # this is the lower limit of the magnitudes to keep
 
-    # this is the directory informations
-    WORKING_DIRECTORY = "D:\\ryanj\\Development\\dfps_sky\\"
-    ANALYSIS_DIRECTORY = WORKING_DIRECTORY + 'analysis\\'
-    LOG_DIRECTORY = WORKING_DIRECTORY + 'logs\\'
+    # this is the directory information
+    WORKING_DIRECTORY = "/home/oelkerrj/Development/dfps_sky/"
+    ANALYSIS_DIRECTORY = WORKING_DIRECTORY + 'analysis/'
+    LOG_DIRECTORY = WORKING_DIRECTORY + 'logs/'
 
     # input paths for data etc
-    DATA_DIRECTORY = WORKING_DIRECTORY + "data\\"
+    DATA_DIRECTORY = WORKING_DIRECTORY + "data/"
 
     # directory_list
     DIRECTORIES = [ANALYSIS_DIRECTORY, DATA_DIRECTORY, LOG_DIRECTORY]
--- scripts.py
+++ scripts.py
@@ -1,5 +1,6 @@
 """ This the scripting function which will hold the basic scripts used to locate the DFPS fibers on the sky."""
 import pandas as pd
+import numpy as np
 import os
 from config import Configuration
 from astroquery.mast import Catalogs
@@ -7,10 +8,11 @@ from utils import Utils
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings('ignore')
 import logging
 logging.getLogger('matplotlib.font_manager').disabled = True
+matplotlib.pyplot.set_loglevel (level = 'warning')
+pil_logger = logging.getLogger('PIL')
+pil_logger.setLevel(logging.INFO)
 
 
 class Scripts:
@@ -53,6 +55,68 @@ class Scripts:
         return catalog_data_clip
 
     @staticmethod
+    def plot_a_box(cen_x, cen_y, sz_x, sz_y):
+        """ This function will return the coordinates of a box that can be plotted.
+
+        :parameter cen_x - this is the x coordinate of the box
+        :parameter cen_y - this is the y coordinate of the box
+        :parameter sz - This is the length of on box side
+
+        :return box_x, box_y - Two numpy arrays are returned which will allow you to plot a box.
+
+        """
+
+        # set up x vertices
+        x1 = cen_x - sz_x / 2.
+        x2 = cen_x + sz_x / 2.
+
+        # set up y vertices
+        y1 = cen_y - sz_y / 2.
+        y2 = cen_y + sz_y / 2.
+
+        # set up the np.arrays
+        box_x = np.array([x1, x1, x2, x2, x1])
+        box_y = np.array([y1, y2, y2, y1, y1])
+
+        return box_x, box_y
+
+    @staticmethod
+    def plot_guide_cameras(cam_x, cam_y, cam_num):
+        """ This function will return the positions to plot the guide cameras based on the camera provided.
+
+        :parameter cam_x - The x center for the camera
+        :parameter cam_y - The y center for the camera
+        :parameter cam_num - The camera you selected on the image
+
+        :return The required positions of all 4 cameras will be returned.
+        """
+
+        # convert the distance between cameras from x/y to ra/dec
+        cam_off = (Configuration.DFPS_GUIDE_CAMERA_DIST * Configuration.OTTO_STRUVE_PLATE_SCALE) / 3600.
+        if cam_num == '1':
+            guide_cam_1_x, guide_cam_1_y = Scripts.plot_a_box(cam_x, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_2_x, guide_cam_2_y = Scripts.plot_a_box(cam_x + cam_off, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_3_x, guide_cam_3_y = Scripts.plot_a_box(cam_x, cam_y - cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_4_x, guide_cam_4_y = Scripts.plot_a_box(cam_x + cam_off, cam_y - cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+        elif cam_num == '2':
+            guide_cam_1_x, guide_cam_1_y = Scripts.plot_a_box(cam_x - cam_off, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_2_x, guide_cam_2_y = Scripts.plot_a_box(cam_x, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_3_x, guide_cam_3_y = Scripts.plot_a_box(cam_x - cam_off, cam_y - cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_4_x, guide_cam_4_y = Scripts.plot_a_box(cam_x, cam_y - cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+        elif cam_num == '3':
+            guide_cam_1_x, guide_cam_1_y = Scripts.plot_a_box(cam_x, cam_y + cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_2_x, guide_cam_2_y = Scripts.plot_a_box(cam_x + cam_off, cam_y + cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_3_x, guide_cam_3_y = Scripts.plot_a_box(cam_x, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_4_x, guide_cam_4_y = Scripts.plot_a_box(cam_x + cam_off, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+        else:
+            guide_cam_1_x, guide_cam_1_y = Scripts.plot_a_box(cam_x - cam_off, cam_y + cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_2_x, guide_cam_2_y = Scripts.plot_a_box(cam_x, cam_y + cam_off, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_3_x, guide_cam_3_y = Scripts.plot_a_box(cam_x - cam_off, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+            guide_cam_4_x, guide_cam_4_y = Scripts.plot_a_box(cam_x, cam_y, Configuration.DFPS_GUIDE_CAMERA_FOV_X, Configuration.DFPS_GUIDE_CAMERA_FOV_Y)
+
+        return guide_cam_1_x, guide_cam_1_y, guide_cam_2_x, guide_cam_2_y, guide_cam_3_x, guide_cam_3_y ,guide_cam_4_x, guide_cam_4_y
+
+    @staticmethod
     def pick_n_plot(stars, offsets, pixel_size, plate_scale):
         """ This function will allow you to plot the searched stars on an x/y plot, place the guide cameras,
         automatically place the fibers, place desired locations, and then return an offset.
@@ -65,73 +129,209 @@ class Scripts:
         :return movements - An np.array of movements is returned also a text file is also output for each readibility.
         """
 
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=[8,6])
 
         # plot the stars in ra dec
-        plt.scatter(stars.ra, stars.dec, marker='*', c='k', s=(20 - stars.mag) * 5)
+        plt.scatter(stars.ra, stars.dec, marker='*', c='k', s=(20 - stars.mag) * 10)
 
-        # add the field of view of the telescope on the frame
-        for x in range(0, 2):
-            xx = [Configuration.RA_DEG + Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2,
-                  Configuration.RA_DEG - Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2]
-            if x == 0:
-                yy = [Configuration.DEC_DEG - Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2,
-                      Configuration.DEC_DEG - Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2]
-            else:
-                yy = [Configuration.DEC_DEG + Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2,
-                      Configuration.DEC_DEG + Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2]
+        # add the field of view of the telescope on the image
+        telescope_fov_x, telescope_fov_y = Scripts.plot_a_box(Configuration.RA_DEG,
+                                                              Configuration.DEC_DEG,
+                                                              Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG,
+                                                              Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG)
+        plt.plot(telescope_fov_x, telescope_fov_y, c='g', label='Telescope')
 
-            plt.plot(xx, yy, c='r')
+        # now add the instrument cameras for reference on the image
+        def on_click_cams(event):
+            if event.button == 3:
+                prompt = "Which camera canter are you trying to place?"
+                cam = input(prompt)
 
-        # add the field of view of the telescope on the frame
-        for x in range(0, 2):
-            yy = [Configuration.DEC_DEG + Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2,
-                  Configuration.DEC_DEG - Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2]
-            if x == 0:
-                xx = [Configuration.RA_DEG - Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2,
-                      Configuration.RA_DEG - Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2]
-            else:
-                xx = [Configuration.RA_DEG + Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2,
-                      Configuration.RA_DEG + Configuration.OTTO_STRUVE_FIELD_OF_VIEW_DEG / 2]
-            plt.plot(xx, yy, c='r')
+                x = event.xdata
+                y = event.ydata
 
-        def on_click(event):
-            if event.button == 3:  # Right click
-                plt.clf()  # Clear the figure
-                plt.draw()  # Redraw
+                # get the positional information for the cameras
+                gc_1x, gc_1y, gc_2x, gc_2y, gc_3x, gc_3y, gc_4x, gc_4y = Scripts.plot_guide_cameras(x, y, cam)
+
+                plt.plot(gc_1x, gc_1y, c='r')
+                plt.text(gc_1x[0], gc_1y[0], 'Guide-1')
+                plt.plot(gc_2x, gc_2y, c='r')
+                plt.text(gc_2x[0], gc_2y[0], 'Guide-2')
+                plt.plot(gc_3x, gc_3y, c='r')
+                plt.text(gc_3x[0], gc_3y[0], 'Guide-3')
+                plt.plot(gc_4x, gc_4y, c='r')
+                plt.text(gc_4x[0], gc_4y[0], 'Guide-4')
                 plt.show()
-            else:
-                ra_cam = event.xdata
-                dec_cam = event.ydata
 
-                # add the field of view of the telescope on the frame
-                for x in range(0, 2):
-                    xx = [ra_cam + Configuration.DFPS_GUIDE_CAMERA_SIZE / 2,
-                          ra_cam - Configuration.DFPS_GUIDE_CAMERA_SIZE / 2]
-                    if x == 0:
-                        yy = [dec_cam - Configuration.DFPS_GUIDE_CAMERA_SIZE / 2,
-                              dec_cam - Configuration.DFPS_GUIDE_CAMERA_SIZE / 2]
-                    else:
-                        yy = [dec_cam + Configuration.DFPS_GUIDE_CAMERA_SIZE / 2,
-                              dec_cam + Configuration.DFPS_GUIDE_CAMERA_SIZE / 2]
+                # now dump the positions to a text file for later
+                f = open(Configuration.ANALYSIS_DIRECTORY + "guide_camera_positions.txt", "w")
+                for idx in range(0, 5):
+                    line = str(gc_1x[idx]) + ' ' + str(gc_1y[idx]) + ' ' + \
+                           str(gc_2x[idx]) + ' ' + str(gc_2y[idx]) + ' ' + \
+                           str(gc_3x[idx]) + ' ' + str(gc_3y[idx]) + ' ' + \
+                           str(gc_4x[idx]) + ' ' + str(gc_4y[idx]) + "\n"
+                    f.write(line)
+                f.close()
 
-                    plt.plot(xx, yy, c='b')
-
-                # add the field of view of the telescope on the frame
-                for x in range(0, 2):
-                    yy = [dec_cam + Configuration.DFPS_GUIDE_CAMERA_SIZE / 2,
-                          dec_cam - Configuration.DFPS_GUIDE_CAMERA_SIZE / 2]
-                    if x == 0:
-                        xx = [ra_cam - Configuration.DFPS_GUIDE_CAMERA_SIZE / 2,
-                              ra_cam - Configuration.DFPS_GUIDE_CAMERA_SIZE / 2]
-                    else:
-                        xx = [ra_cam + Configuration.DFPS_GUIDE_CAMERA_SIZE / 2,
-                              ra_cam + Configuration.DFPS_GUIDE_CAMERA_SIZE / 2]
-                    plt.plot(xx, yy, c='b')
-
-        cid = fig.canvas.mpl_connect('button_press_event', on_click)
+        cid = fig.canvas.mpl_connect('button_press_event', on_click_cams)
 
         plt.ylabel('Declination [deg]')
         plt.xlabel('Right Ascension [deg]')
+        plt.gca().invert_xaxis()
+        plt.legend()
         plt.show()
+        plt.close()
 
+        fig, ax = plt.subplots(figsize=[8,6])
+
+        # re-plot for the star pickers
+        plt.scatter(stars.ra, stars.dec, marker='*', c='k', s=(20 - stars.mag) * 10)
+        plt.plot(telescope_fov_x, telescope_fov_y, c='g', label='Telescope')
+
+        # re-plot the camera positions
+        gc = pd.read_csv(Configuration.ANALYSIS_DIRECTORY + 'guide_camera_positions.txt', sep=' ',
+                         names=['gc_1x', 'gc_1y', 'gc_2x', 'gc_2y','gc_3x', 'gc_3y', 'gc_4x', 'gc_4y'])
+
+        plt.plot(gc.gc_1x, gc.gc_1y, c='r', label='Guide Cameras')
+        plt.text(gc.gc_1x[0], gc.gc_1y[0], 'Guide-1')
+        plt.plot(gc.gc_2x, gc.gc_2y, c='r')
+        plt.text(gc.gc_2x[0], gc.gc_2y[0], 'Guide-2')
+        plt.plot(gc.gc_3x, gc.gc_3y, c='r')
+        plt.text(gc.gc_3x[0], gc.gc_3y[0], 'Guide-3')
+        plt.plot(gc.gc_4x, gc.gc_4y, c='r')
+        plt.text(gc.gc_4x[0], gc.gc_4y[0], 'Guide-4')
+        plt.legend()
+
+        def on_click_fibers(event):
+            if event.button == 3:  # Left click
+
+                prompt = "Which camera is the star you are trying to place in?"
+                cam = input(prompt)
+
+                if cam == '1':
+                    off_x = Configuration.OFFSET_X_1
+                    off_y = Configuration.OFFSET_Y_1
+                elif cam == '2':
+                    off_x = Configuration.OFFSET_X_2
+                    off_y = Configuration.OFFSET_Y_2
+                elif cam == '3':
+                    off_x = Configuration.OFFSET_X_3
+                    off_y = Configuration.OFFSET_Y_3
+                else:
+                    off_x = Configuration.OFFSET_X_4
+                    off_y = Configuration.OFFSET_Y_4
+
+                # get the data from the figure
+                global x, y
+                x = event.xdata
+                y = event.ydata
+
+                # get the camera verticies
+                plt.scatter(x, y, c='r', marker='.')
+
+                # now plot the fiber on the frame
+
+                offset_x = (Configuration.OTTO_STRUVE_PLATE_SCALE * off_x) / 3600
+                offset_y = (Configuration.OTTO_STRUVE_PLATE_SCALE * off_y) / 3600
+
+                plt.scatter(x + offset_x, y + offset_y, c='b')
+                plt.show()
+
+                # now dump the fiber position to a text file
+                # now dump the positions to a text file for later
+                f = open(Configuration.ANALYSIS_DIRECTORY + "camera_" + cam + "_fiber_position.txt", "w")
+                f.write(str(x + offset_x) + " " + str(y + offset_y) + "\n")
+                f.close()
+
+        cid = fig.canvas.mpl_connect('button_press_event', on_click_fibers)
+
+        plt.ylabel('Declination [deg]')
+        plt.xlabel('Right Ascension [deg]')
+        plt.gca().invert_xaxis()
+        plt.show()
+        plt.close()
+
+        fig, ax = plt.subplots(figsize=[8,6])
+
+        # re-plot for the star pickers
+        plt.scatter(stars.ra, stars.dec, marker='*', c='k', s=(20 - stars.mag) * 10)
+        plt.plot(telescope_fov_x, telescope_fov_y, c='g', label='Telescope')
+
+        # re-plot the camera positions
+        plt.plot(gc.gc_1x, gc.gc_1y, c='r', label='Guide Cameras')
+        plt.text(gc.gc_1x[0], gc.gc_1y[0], 'Guide-1')
+        plt.plot(gc.gc_2x, gc.gc_2y, c='r')
+        plt.text(gc.gc_2x[0], gc.gc_2y[0], 'Guide-2')
+        plt.plot(gc.gc_3x, gc.gc_3y, c='r')
+        plt.text(gc.gc_3x[0], gc.gc_3y[0], 'Guide-3')
+        plt.plot(gc.gc_4x, gc.gc_4y, c='r')
+        plt.text(gc.gc_4x[0], gc.gc_4y[0], 'Guide-4')
+
+        # re-plot the current fiber positions
+        fiber1 = pd.read_csv(Configuration.ANALYSIS_DIRECTORY + 'camera_1_fiber_position.txt', sep=' ',
+                         names=['fb_x', 'fb_y'])
+        fiber2 = pd.read_csv(Configuration.ANALYSIS_DIRECTORY + 'camera_2_fiber_position.txt', sep=' ',
+                         names=['fb_x', 'fb_y'])
+        fiber3 = pd.read_csv(Configuration.ANALYSIS_DIRECTORY + 'camera_3_fiber_position.txt', sep=' ',
+                         names=['fb_x', 'fb_y'])
+        fiber4 = pd.read_csv(Configuration.ANALYSIS_DIRECTORY + 'camera_4_fiber_position.txt', sep=' ',
+                         names=['fb_x', 'fb_y'])
+
+        plt.scatter(fiber1.fb_x, fiber1.fb_y, c='b', label='Fibers')
+        plt.text(fiber1.fb_x[0], fiber1.fb_y[0], 'Fiber-1')
+        plt.scatter(fiber2.fb_x, fiber2.fb_y, c='b')
+        plt.text(fiber2.fb_x[0], fiber2.fb_y[0], 'Fiber-2')
+        plt.scatter(fiber3.fb_x, fiber3.fb_y, c='b')
+        plt.text(fiber3.fb_x[0], fiber3.fb_y[0], 'Fiber-3')
+        plt.scatter(fiber4.fb_x, fiber4.fb_y, c='b')
+        plt.text(fiber4.fb_x[0], fiber4.fb_y[0], 'Fiber-4')
+
+        # now we want to select the stars we want to move to
+        def on_click_stars(event):
+            if event.button == 3:  # Left click
+
+                prompt = "Which fiber do you want to move to this star?"
+                fiber = input(prompt)
+
+                if fiber == '1':
+                    fb_x = fiber1.fb_x[0]
+                    fb_y = fiber1.fb_y[0]
+                elif fiber == '2':
+                    fb_x = fiber2.fb_x[0]
+                    fb_y = fiber2.fb_y[0]
+                elif fiber == '3':
+                    fb_x = fiber3.fb_x[0]
+                    fb_y = fiber3.fb_y[0]
+                else:
+                    fb_x = fiber4.fb_x[0]
+                    fb_y = fiber4.fb_y[0]
+
+                # get the data from the figure
+                global x, y
+                x = event.xdata
+                y = event.ydata
+
+                # get the camera verticies
+                plt.scatter(x, y, s=80, facecolors='none', edgecolors='gold')
+
+                # now get the offset in mm
+                offset_x = ((x - fb_x) * 3600) / Configuration.OTTO_STRUVE_PLATE_SCALE
+                offset_y = ((y - fb_y) * 3600) / Configuration.OTTO_STRUVE_PLATE_SCALE
+
+                plt.text(x, y, 'Fiber ' + fiber + ' Offset X: ' + str(np.around(offset_x, decimals=3)) + "mm Y: " + str(np.around(offset_y, decimals=3)) + "mm")
+                plt.show()
+
+                f = open(Configuration.ANALYSIS_DIRECTORY + "fiber_" + fiber + "_change.txt", "w")
+                f.write(str(x + offset_x) + " " + str(y + offset_y) + "\n")
+                f.close()
+
+
+        cid = fig.canvas.mpl_connect('button_press_event', on_click_stars)
+
+        plt.legend(loc='upper right')
+        plt.ylabel('Declination [deg]')
+        plt.xlabel('Right Ascension [deg]')
+        plt.gca().invert_xaxis()
+        plt.show()
+        plt.close()
+        print('hold')
